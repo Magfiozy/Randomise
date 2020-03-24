c9976deb49375c9efc8c328340a0f0b37528de43
Magfiozy777 <34837894+Magfiozy777@users.noreply.github.com>
2020-03-24 16:12:36 +0200

    initial

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            SetRandomNumber();
+        }
+
+
+        private async void SetRandomNumber()
+        {
+            HttpClient client = new HttpClient();
+            double resultNumber = 0;
+
+            await client.GetAsync(@"http://localhost:10823/api/randomNumbers")
+                .ContinueWith(response =>
+                {
+                    if (response.Exception != null)
+                    {
+                        MessageBox.Show(response.Exception.Message);
+                    }
+                    else
+                    {
+                        HttpResponseMessage message = response.Result;
+
+                        Double.TryParse(message.Content.ReadAsStringAsync().Result, out resultNumber);
+                    }
+                });
+
+            slValue.Value = resultNumber;
+            labelValue.Content = resultNumber.ToString();
+        }
+    }
+}
---
+++ Randomise/Controllers/RandomNumbersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Randomise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomNumbersController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<int> Get()
+        {
+            Random rnd = new Random();
+            return Ok(rnd.Next(1,100));
+        }
+
+    }
+}
